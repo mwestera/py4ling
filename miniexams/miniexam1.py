28a355122eccc9c6deae78f2cee6439e2620d491
mwestera <matthijs.westera@gmail.com>
2022-02-16 16:54:57 +0100

    add miniexam1

---
+++ miniexams/miniexam1.py
@@ -0,0 +1,22 @@
+## Mini-exam 1
+
+# 1. What is the output of the following program:
+
+name = 'Sue'
+
+print(len(name))
+print(len(name) * 2)
+print(len(name * 2))
+
+print(len('name') - 1)
+print(len('name' * 3))
+print(len('name * 3'))
+print(len('name') == 2)
+
+print(name[1])
+print(name[-1])
+print(name + name[-1] * 4)
+print(name[::-1])
+
+print(24 % 12)
+print(24 % 10)
