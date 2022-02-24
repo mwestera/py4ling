b60181f4dc93229e3e831594e3038b32b11b2e59
mwestera <matthijs.westera@gmail.com>
2022-02-24 16:25:07 +0100

    add twitter_covid_sample.csv

--- adventures/weeks3-4-covid.py
+++ adventures/weeks3-4-covid.py
@@ -3,18 +3,20 @@ import pandas as pd
 
 """
 Code for the VaxSentimentizer3000©
-This is currently just a squib. We will start filling in more and more of this file in class in the weeks the come.
+This is currently just a squib. It will be completed in class in week 4.
 """
+
+
+path_to_our_data = '../data/twitter_covid.csv'  # move up, then down into the data folder
+
+# NOTE: If you want to run this at home, uncomment this (twitter does not allow broad sharing of tweet contents):
+# path_to_our_data = '../data/twitter_covid_sample.csv'
 
 
 # Read the csv file
 # 1. Can I solve this quite easily? No.
 # 2. Is it likely that someone else has solved this? Very.
 # Hence we google, e.g., 'how to read a csv file using python'
-
-path_to_our_data = '../data/twitter_covid.csv'  # move up, then down into data
-
-# NOTE: This .csv file is not available here on github. Twitter does not allow broad sharing of tweet contents.
 
 dataset = pd.read_csv(path_to_our_data)
 
@@ -23,14 +25,11 @@ dataset = pd.read_csv(path_to_our_data)
 #   - do we have tweets from all time points?
 #   - any non-covid tweets?
 #   - filter out empty/weird/unanalyzable tweets
-
-print(dataset['created_at'].to_string())
-
 # 1. can we do this easily ourselves? Nope.
 # 2. Is it likely that someone has solved this before us? Yes, very!
 #      What should we google for?   [to be continued!]
 
-
+print(dataset['created_at'].to_string())
 
 
 # Take the text, compute its vaccine sentiment as a float, e.g., using:
