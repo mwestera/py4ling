8405714cb6b32339cce9cc7f31dc5babad6d4685
mwestera <matthijs.westera@gmail.com>
2022-05-04 17:09:42 +0200

    add gensim and semantle; cleanup post-class

--- adventures/weeks8-12_similarity.py
+++ adventures/weeks8-12_similarity.py
@@ -7,6 +7,8 @@ import os
 import gensim
 import gensim.downloader as api
 
+import random
+
 """
 1. load a big-ish text corpus
 2. represent each word by some sort of mathematical object representative of the contexts in which it occurs
@@ -18,10 +20,20 @@ import gensim.downloader as api
 
 N_TOKENS = 500
 WINDOW = 20
-REDO = True # Whether to recompute the cooccurrence counts if the file already exists
-SAVE_PATH = 'cooccurrences.csv'
+REDO = False # Whether to recompute the cooccurrence counts if the file already exists
+SAVE_PATH = 'cooccurrences.pkl'
+USE_GENSIM = True
+CUSTOM_WORD2VEC = False # only if USE_GENSIM; if False, uses Google's model.
+PLAY_SEMANTLE_AFTERWARDS = True # only if USE_GENSIM
 
 def main():
+    if USE_GENSIM:
+        gensim_variant()
+    else:
+        manual_variant()
+
+
+def manual_variant():
 
     if REDO or not os.path.exists(SAVE_PATH):
         tokens = [tok.lower() for tok in brown.words()]
@@ -29,27 +41,67 @@ def main():
         matrix = cooccurrence_matrix(tokens, tokens_to_use, WINDOW)
         matrix.to_csv(SAVE_PATH)
     else:
-        matrix = pd.read_csv(SAVE_PATH)
+        matrix = pd.read_csv(SAVE_PATH, index_col=0)
+
+    # wv = dimensionality_reduction(matrix)     # Not done yet
+    wv = matrix
 
     for word in ['young', 'man', 'woman', 'person', 'car', 'love', 'hand']:
-        print(word, most_similar(word, matrix))
+        print(word, most_similar(word, wv))
 
-## Added for class 10:
-# def main_gensim():
-#     if REDO or not os.path.exists(SAVE_PATH):
-#         model = gensim.models.Word2Vec(sentences=brown.sents())
-#         model.save(SAVE_PATH)
-#     else:
-#         model = gensim.models.Word2Vec.load(SAVE_PATH)
-#
-#     wv = model.wv
-#
-#     # wv = api.load('word2vec-google-news-300')  # 1.6 GB!
-#
-#     for word in ['young', 'man', 'woman', 'person', 'car', 'love', 'hand']:
-#         print(word, wv.most_similar(word))
-#
-#     # TODO: analogical reasoning
+
+def gensim_variant():
+    if CUSTOM_WORD2VEC:
+        wv = get_custom_word2vec()
+    else:
+        wv = get_google_word2vec()
+
+    for word in ['young', 'man', 'woman', 'person', 'car', 'love', 'hand']:
+        print(word, wv.most_similar(word))
+
+    print(wv.most_similar('king'))
+    print(wv.most_similar(positive=['king', 'woman'], negative=['man']))    # king - man + woman
+    print(wv.most_similar(positive=['king', 'woman', 'woman', 'woman'], negative=['man']))    # king - man + woman
+
+    if PLAY_SEMANTLE_AFTERWARDS:
+        semantle(wv)
+
+def get_custom_word2vec():
+    if REDO or not os.path.exists(SAVE_PATH):
+        model = gensim.models.Word2Vec(sentences=brown.sents())
+        model.save(SAVE_PATH)
+    else:
+        model = gensim.models.Word2Vec.load(SAVE_PATH)
+
+    wv = model.wv
+
+    return wv
+
+def get_google_word2vec():
+    wv = api.load('word2vec-google-news-300')  # 1.6 GB!
+    return wv
+
+
+def semantle(wv):
+    while True:
+        secret_word = random.choice(wv.index_to_key)
+        if secret_word == secret_word.lower():
+            break
+    print(secret_word)
+    n_attempts = 0
+    while True:
+        guess = input('Guess!').lower()
+        n_attempts += 1
+        if guess == secret_word:
+            print(f'Wooohoooh! In {n_attempts} attempts!')
+            break
+        try:
+            similarity = wv.similarity(secret_word, guess)
+        except:
+            print('Word not found.')
+            continue
+        print('   ', similarity)
+
 
 def determine_words_we_will_use(tokens, nwords=None):
     """
